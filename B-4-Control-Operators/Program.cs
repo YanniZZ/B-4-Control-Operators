689060ed0ea297394dc74b7f7a7b86a26dbf9703
Ян Золоторевич <yanzolotorevich@gmail.com>
2018-12-14 19:43:31 +0300

    Add project files.

---
+++ B-4-Control-Operators/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.Lesson_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Practice.B3_P2_9_CheckResultAddition();
+             Console.ReadLine();
+        }
+     }
+}
